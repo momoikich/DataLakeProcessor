52e84187e1a4f9455a60fc519f24cce269680b0b
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2023-12-16 15:37:39 +0100

    add fonctionnalities

--- src/main/scala/DataLakeProcessor.scala
+++ src/main/scala/DataLakeProcessor.scala
@@ -2,15 +2,18 @@ package com.example
 
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.Window
 
 object DataLakeProcessor {
 
   def main(args: Array[String]): Unit = {
     // Initialize SparkSession
     val spark = SparkSession.builder
-      .appName("DynamicDataLake")
-      .master("local[*]")
-      .getOrCreate()
+        .appName("DataLakeProcessor")
+        .master("local[*]")
+        .config("spark.hadoop.fs.defaultFS", "file:///")
+        .getOrCreate()
+
 
     // Ingest data (Replace this with your actual data source)
     val rawData: DataFrame = spark.read.option("header", "true").csv(raw"C:\Users\hammi\OneDrive\Bureau\hello-world\src\main\scala\csv\file.csv")
@@ -29,6 +32,25 @@ object DataLakeProcessor {
 
     // Display the aggregated data
     aggregatedData.show()
+
+    // Data Enrichment
+    val enrichedData: DataFrame = enrichData(cleanData)
+
+    // Display the enriched data
+    enrichedData.show()
+
+    // Data Partitioning
+    val partitionedData: DataFrame = partitionData(cleanData)
+
+    // Display the partitioned data
+    partitionedData.show()
+
+    // Window Function for Analysis
+    val windowSpec = Window.partitionBy("City").orderBy(desc("Age"))
+    val rankData: DataFrame = cleanData.withColumn("Rank", rank().over(windowSpec))
+
+    // Display data with rank
+    rankData.show()
 
     // Stop SparkSession
     spark.stop()
@@ -50,6 +72,14 @@ object DataLakeProcessor {
       .groupBy("City")
       .agg(count("*").alias("Count"))
   }
+
+  def enrichData(data: DataFrame): DataFrame = {
+    // Example Data Enrichment: Add a new column "IsAdult"
+    data.withColumn("IsAdult", when(col("Age").cast("int") >= 18, "Yes").otherwise("No"))
+  }
+
+  def partitionData(data: DataFrame): DataFrame = {
+    // Example Data Partitioning: Partition data by the "City" column
+    data.repartition(col("City"))
+  }
 }
-
-
